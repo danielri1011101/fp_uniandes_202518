102954f838ab7555aa196a2472d66c68268621ab
danielri1011101 <barreroroserodr@gmail.com>
2025-07-11 17:46:27 -0500

    Function for getting nth element.

--- Clase1.hs
+++ Clase1.hs
@@ -1,3 +1,5 @@
+import Control.Monad.Accum (MonadAccum(accum))
+import System.Win32 (COORD(xPos))
 pow :: Num a => a -> Int -> a
 pow = \ x n -> powIter x n 1
   where
@@ -14,3 +16,21 @@ myLen xs = iterLen xs 0
   where
     iterLen [] accum = accum
     iterLen (x:xs) accum = iterLen xs (1+accum)
+
+pullElem :: Num a => Int -> [a] -> a
+pullElem n xs
+  | n < 0 = -1
+  | otherwise = iterElem (n+1) xs (-1)
+    where
+        iterElem 0 _ accum = accum
+        iterElem n [] _ = -1
+        iterElem n (x:xs) _ = iterElem (n-1) xs x
+
+pullElem' :: Int -> [a] -> Maybe a
+pullElem' n xs
+  | n < 0 = Nothing
+  | otherwise = iterElem' (n+1) xs Nothing
+    where
+        iterElem' 0 _ accum = accum
+        iterElem' n [] _ = Nothing
+        iterElem' n (x:xs) _ = iterElem' (n-1) xs (Just x)
