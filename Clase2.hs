5162b100a1382a0b315112813fc1e41a3cff13e8
danielri1011101 <barreroroserodr@gmail.com>
2025-07-14 16:09:34 -0500

    Some partial applications.

--- Clase2.hs
+++ Clase2.hs
@@ -10,3 +10,25 @@ div' = div
 
 mod' :: Int -> Int -> Int
 mod' = \ a m -> a - a `div` m
+
+-- Making a non-standard instance of Num.
+instance Num a => Num (b -> a) where
+    f + g = \ x -> f x + g x
+    f * g = \ x -> f x * g x
+    f - g = \ x -> f x - g x
+    signum = id
+    abs f = abs . f
+    fromInteger n = \ x -> fromInteger n
+
+multThree :: Num a => a -> a -> a -> a
+multThree x y z = x * y * z
+
+-- multThree 4 :: Num a => a -> a -> a
+
+multThree' :: Num a => a -> a -> a -> a
+multThree' x = \ y z -> x * y * z
+
+applyTwice' :: (a -> a) -> (a -> a)
+applyTwice' = \ f -> f . f
+
+-- compare (-8) :: (Num a, Ord a) => a -> Ordering
