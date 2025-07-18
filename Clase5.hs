d1b9daaf84112a8cf7c2bfbb3167f4d3c8daac2f
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-07-18 10:56:57 -0500

    Added Wolf's code from yesterday's class.

--- Clase5.hs
+++ Clase5.hs
@@ -83,36 +83,3 @@ main = do
 
 when1 :: Bool -> IO () -> IO ()
 when1 = \ b ac -> if b then ac else return ()
-
--- More of Wolf's code
-
-res1 = liftA2 (+) [1,2,3] [4,5,6] 
-res2 = liftA2 (+) (Just 10) (Just 22)
-res3 = liftA2 (+) (Right 10) (Right 22)
-
-applicativeCode a1 a2 = liftA2 (+) a1 a2
-
-res1' = applicativeCode [1,2,3] [4,5,6]
-res2' = applicativeCode (Just 10) (Just 22)
-res3' = applicativeCode (Right 10) (Right 22)
-
-data Logged a = L a String deriving (Show)
-
-instance Functor Logged where
-  fmap f (L x s) = L (f x) s
-  
-instance Applicative Logged where
-  pure = flip L ""
-  (L f s) <*> (L a s') = L (f a) (s++s')
-  
-logg x m = L x m
-  
-se = sequenceA [pure (fac 3), 
-                logg 40 "fourty",
-                applicativeCode (pure 45) 
-                                (logg 10 "ten"),
-                logg (fac 3) "three"]
-                
-ff x = if even x then pure x else logg x (show x)
-
-sw = sequenceA [ff x | x <- [1..10]]
